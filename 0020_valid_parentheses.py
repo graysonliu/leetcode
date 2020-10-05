94ff8bdc18dfe1edd0c4fd84045e2efc437aa460
Zijian Liu <liu.zijian@outlook.com>
2020-10-05 03:16:27 -0400

    :running:

--- 0020_valid_parentheses.py
+++ 0020_valid_parentheses.py
@@ -1,48 +1,52 @@
-# Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 #
 # An input string is valid if:
 #
 # Open brackets must be closed by the same type of brackets.
 # Open brackets must be closed in the correct order.
-# Note that an empty string is also considered valid.
+#
 #
 # Example 1:
 #
-# Input: "()"
+# Input: s = "()"
 # Output: true
 # Example 2:
 #
-# Input: "()[]{}"
+# Input: s = "()[]{}"
 # Output: true
 # Example 3:
 #
-# Input: "(]"
+# Input: s = "(]"
 # Output: false
 # Example 4:
 #
-# Input: "([)]"
+# Input: s = "([)]"
 # Output: false
 # Example 5:
 #
-# Input: "{[]}"
+# Input: s = "{[]}"
 # Output: true
+#
+#
+# Constraints:
+#
+# 1 <= s.length <= 10^4
+# s consists of parentheses only '()[]{}'.
+
 
 class Solution:
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        # 简单的栈管理，没什么好说的
-        pair = {'(': ')', '{': '}', '[': ']'}
+    def isValid(self, s: str) -> bool:
+        # using stack
         stack = []
+        pairs = {'(': ')', '[': ']', '{': '}'}
         for c in s:
-            if stack and pair.get(stack[-1], None) == c:
-                stack.pop(-1)
-            else:
+            if c in pairs:
                 stack.append(c)
-        return not stack
+            elif len(stack) > 0 and c == pairs[stack[-1]]:
+                stack.pop()
+            else:
+                return False
+        return len(stack) == 0
 
 
-if __name__ == '__main__':
-    print(list(map(Solution().isValid, ["()", "(]", "{[]}"])))
+print(list(map(Solution().isValid, ["()", "(]", "{[]}"])))
--- 0021_merge_two_sorted_lists.py
+++ 0021_merge_two_sorted_lists.py
@@ -1,62 +1,44 @@
-# Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+# Merge two sorted linked lists and return it as a new sorted list. The new list should be made by splicing together the nodes of the first two lists.
 #
-# Example:
 #
-# Input: 1->2->4, 1->3->4
-# Output: 1->1->2->3->4->4
+#
+# Example 1:
+#
+#
+# Input: l1 = [1,2,4], l2 = [1,3,4]
+# Output: [1,1,2,3,4,4]
+# Example 2:
+#
+# Input: l1 = [], l2 = []
+# Output: []
+# Example 3:
+#
+# Input: l1 = [], l2 = [0]
+# Output: [0]
+#
+#
+# Constraints:
+#
+# The number of nodes in both lists is in the range [0, 50].
+# -100 <= Node.val <= 100
+# Both l1 and l2 are sorted in non-decreasing order.
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+from utils import *
 
 
 class Solution:
-    def mergeTwoLists(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         dummy = ListNode(0)
-        p, p1, p2 = dummy, l1, l2
+        p = dummy
+        p1, p2 = l1, l2
         while p1 and p2:
-            if p1.val < p2.val:
-                p.next = p1
-                p1 = p1.next
+            if p1.val <= p2.val:
+                p.next, p1 = p1, p1.next
             else:
-                p.next = p2
-                p2 = p2.next
+                p.next, p2 = p2, p2.next
             p = p.next
-        if p1:
-            p.next = p1
-        else:
-            p.next = p2
+        p.next = p1 if p1 else p2
         return dummy.next
 
-    # 递归的方法，有点难想啊
-    def mergeTwoLists1(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
 
-        if l1 and l2:
-            if l1.val > l2.val:
-                l1, l2 = l2, l1
-            l1.next = self.mergeTwoLists(l1.next, l2)
-        return l1 or l2
-
-
-if __name__ == '__main__':
-    l1, l1.next, l1.next.next = ListNode(1), ListNode(2), ListNode(4)
-    l2, l2.next, l2.next.next = ListNode(1), ListNode(3), ListNode(4)
-    print(Solution().mergeTwoLists(l1, l2))
+print(linked_list_to_str(Solution().mergeTwoLists(str_to_linked_list('1->2->4'), str_to_linked_list('1->3->4'))))
