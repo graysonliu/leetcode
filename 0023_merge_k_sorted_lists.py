71f7f67174f0126ebd0f9e181932ea157fc764d0
Zijian Liu <liu.zijian@outlook.com>
2020-10-06 06:01:57 -0400

    :running:

--- 0023_merge_k_sorted_lists.py
+++ 0023_merge_k_sorted_lists.py
@@ -1,58 +1,82 @@
+# You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
 #
-# Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+# Merge all the linked-lists into one sorted linked-list and return it.
 #
-# Example:
 #
-# Input:
+#
+# Example 1:
+#
+# Input: lists = [[1,4,5],[1,3,4],[2,6]]
+# Output: [1,1,2,3,4,4,5,6]
+# Explanation: The linked-lists are:
 # [
-#   1->4->5,
-#   1->3->4,
-#   2->6
+#     1->4->5,
+#           1->3->4,
+#                 2->6
 # ]
-# Output: 1->1->2->3->4->4->5->6
+# merging them into one sorted list:
+# 1->1->2->3->4->4->5->6
+# Example 2:
+#
+# Input: lists = []
+# Output: []
+# Example 3:
+#
+# Input: lists = [[]]
+# Output: []
+#
+#
+# Constraints:
+#
+# k == lists.length
+# 0 <= k <= 10^4
+# 0 <= lists[i].length <= 500
+# -10^4 <= lists[i][j] <= 10^4
+# lists[i] is sorted in ascending order.
+# The sum of lists[i].length won't exceed 10^4.
+from typing import List
 
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+from utils import *
 
 
 class Solution:
-    # 前面做过两个链表融合的，直接用那道题的方法逐个融合
-    def mergeKLists1(self, lists):
-        """
-        :type lists: List[ListNode]
-        :rtype: ListNode
-        """
-        from functools import reduce
-        return reduce(self.merge_two_lists, lists, None)
-        # 交上去超时
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        # using heap for k-way merge
+        # heapq.merge() in python receives iterables rather than linked list
+        # we can first create iterables using values of these linked lists, then use heapq.merge() to merge them
+        # finally construct a new linked list using the merged iterable and return it
+        # or we can implement our own version of merge() by using the PriorityQueue
+        # PriorityQueue is also just a heap
+        from queue import PriorityQueue
+        dummy = ListNode(0)
+        p = dummy
+        Q = PriorityQueue()
+        # from on the source code of heapq.merge(), it firstly add all heads of iterables to the heap
+        for head in lists:
+            if head is not None:
+                Q.put((head.val, id(head), head))
+                # id(node) is used as a tie breaker
+                # See https://docs.python.org/3.7/library/heapq.html#priority-queue-implementation-notes
+                # in above doc, it says we might need to add tuple (priority, entry count, task) to the PriorityQueue
+                # the entry count serves as a tie-breaker
+                # so that two tasks with the same priority are returned in the order they were added
+                # that is how we guarantee sort stability
+                # but we don't care sort stability here, we just need a tie-breaker
+                # we use id(node) as the tie-breaker
+                # if we omit id(node) and only add tuple (node.val, node) to the PriorityQueue
+                # the program will encounter an error when there are two nodes with the same value (priority)
+                # because our PriorityQueue lacks a tie-breaker
+                pass
 
-    # 还是用两个链表融合的方法，但不是逐个融合，而是用分治
-    def mergeKLists(self, lists):
-        import sys
-        sys.setrecursionlimit(100000)  # 设置递归栈大小
-        # leetcode上的python环境递归深度最多只能为1000，对于某些变态的test case，1000不够，所以设大点
-        if not lists:
-            return []
-        if len(lists) == 1:
-            return lists[0]
-        else:
-            return self.merge_two_lists(self.mergeKLists(lists[:len(lists) // 2]),
-                                        self.mergeKLists(lists[len(lists) // 2:]))
-
-    def merge_two_lists(self, l1, l2):
-        if l1 and l2:
-            if l1.val > l2.val:
-                l1, l2 = l2, l1
-            l1.next = self.merge_two_lists(l1.next, l2)
-        return l1 or l2
+        while not Q.empty():
+            _, _, p.next = Q.get()
+            p = p.next
+            if p.next is not None:
+                Q.put((p.next.val, id(p.next), p.next))
+        return dummy.next
 
 
-if __name__ == "__main__":
-    l1, l1.next, l1.next.next = ListNode(1), ListNode(4), ListNode(5)
-    l2, l2.next, l2.next.next = ListNode(1), ListNode(3), ListNode(4)
-    l3, l3.next = ListNode(2), ListNode(6)
-    lists = [l1, l2, l3]
-    Solution().mergeKLists(lists)
+l1 = str_to_linked_list('1->4->5')
+l2 = str_to_linked_list('1->3->4')
+l3 = str_to_linked_list('2->6')
+print(linked_list_to_str(Solution().mergeKLists([l1, l2, l3])))
