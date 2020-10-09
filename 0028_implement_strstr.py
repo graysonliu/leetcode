14370a6d6a09f0976f964f302f7befe0d232024c
Zijian Liu <liu.zijian@outlook.com>
2020-10-09 02:00:50 -0400

    :running:

--- 0028_implement_strstr.py
+++ 0028_implement_strstr.py
@@ -2,6 +2,14 @@
 #
 # Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 #
+# Clarification:
+#
+# What should we return when needle is an empty string? This is a great question to ask during an interview.
+#
+# For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+#
+#
+#
 # Example 1:
 #
 # Input: haystack = "hello", needle = "ll"
@@ -10,48 +18,24 @@
 #
 # Input: haystack = "aaaaa", needle = "bba"
 # Output: -1
-# Clarification:
+# Example 3:
 #
-# What should we return when needle is an empty string? This is a great question to ask during an interview.
+# Input: haystack = "", needle = ""
+# Output: 0
 #
-# For the purpose of this problem, we will return 0 when needle is an empty string.
-# This is consistent to C's strstr() and Java's indexOf().
+#
+# Constraints:
+#
+# 0 <= haystack.length, needle.length <= 5 * 10^4
+# haystack and needle consist of only lower-case English characters.
+
 
 class Solution:
-    def strStr(self, haystack, needle):
-        return haystack.find(needle)
-
-    def strStr1(self, haystack, needle):
+    def strStr(self, haystack: str, needle: str) -> int:
         for i in range(len(haystack) - len(needle) + 1):
             if haystack[i:i + len(needle)] == needle:
                 return i
         return -1
 
-    def strStr2(self, haystack, needle):
-        """
-        :type haystack: str
-        :type needle: str
-        :rtype: int
-        """
 
-        # 递归的方法很蠢，交上去内存溢出
-        def match_first_char(str1, str2):
-            return str1 and str1[0] == str2[0]
-
-        def match_str(str1, str2):
-            if not str2:
-                return True
-            return match_first_char(str1, str2) and match_str(str1[1:], str2[1:])
-
-        if not needle:
-            return 0
-
-        for i in range(len(haystack)):
-            if match_str(haystack[i:], needle):
-                return i
-
-        return -1
-
-
-if __name__ == '__main__':
-    print(Solution().strStr("hello", "as"))
+print(Solution().strStr("hello", "as"))
--- 0029_divide_two_integers.py
+++ 0029_divide_two_integers.py
@@ -1,56 +1,66 @@
-# Given two integers dividend and divisor, divide two integers without using multiplication, division and mod operator.
+# Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
 #
 # Return the quotient after dividing dividend by divisor.
 #
-# The integer division should truncate toward zero.
+# The integer division should truncate toward zero, which means losing its fractional part. For example, truncate(8.345) = 8 and truncate(-2.7335) = -2.
+#
+# Note:
+#
+# Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−2^31,  2^31 − 1]. For this problem, assume that your function returns 2^31 − 1 when the division result overflows.
+#
 #
 # Example 1:
 #
 # Input: dividend = 10, divisor = 3
 # Output: 3
+# Explanation: 10/3 = truncate(3.33333..) = 3.
 # Example 2:
 #
 # Input: dividend = 7, divisor = -3
 # Output: -2
-# Note:
+# Explanation: 7/-3 = truncate(-2.33333..) = -2.
+# Example 3:
 #
-# Both dividend and divisor will be 32-bit signed integers.
-# The divisor will never be 0.
-# Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−2^31,  2^31 − 1].
-# For the purpose of this problem, assume that your function returns 2^31 − 1 when the division result overflows.
+# Input: dividend = 0, divisor = 1
+# Output: 0
+# Example 4:
+#
+# Input: dividend = 1, divisor = 1
+# Output: 1
+#
+#
+# Constraints:
+#
+# -2^31 <= dividend, divisor <= 2^31 - 1
+# divisor != 0
 
 class Solution:
-    def divide(self, dividend, divisor):
-        """
-        :type dividend: int
-        :type divisor: int
-        :rtype: int
-        """
-        # 回忆一下C和Python中整除的区别，C中的整除向零取整，Python中的整除向负无穷取整
-        # 正常的思路很简单，被除数不断减去除数即可
-        # 但当除数很小（比如除数为1），而被除数又很大时，每次减一个除数太慢了
+    def divide(self, dividend: int, divisor: int) -> int:
+        # we use bit manipulations
 
-        # 首先确定商的符号
-        positive = (dividend > 0) == (divisor > 0)
+        # the basic idea is to subtract divisor from the dividend
+        # but only subtract 1*divisor from dividend is inefficient
+        # we should subtract multi-times of divisor from dividend by bit manipulation
+        # for example, divisor<<1 becomes 2*divisor
 
+        # check whether the quotient is positive
+
+        # the only overflow is when dividend=INT_MIN and divisor=-1
+        if dividend == -2 ** 31 and divisor == -1:
+            return 2 ** 31 - 1
+        pos = (dividend > 0) == (divisor > 0)
         dividend, divisor = abs(dividend), abs(divisor)
         quotient = 0
 
         while dividend >= divisor:
-            multi_divisor, increment_quot = divisor, 1
+            multi_divisor = divisor  # start from 1*divisor, also happens when divisor <= dividend < multi_divisor
+            increment = 1  # this is to record how many times of divisor we subtract from dividend each time
             while dividend >= multi_divisor:
                 dividend -= multi_divisor
-                quotient += increment_quot
-                # 移位操作，每次减的除数翻倍，所以商的增量也翻倍
-                multi_divisor <<= 1  # 这里是每次左移一位，即每次乘2，每次左移更多位数其实也可以
-                increment_quot <<= 1
-
-        if not positive:
-            quotient = -quotient
-
-        inf = 2 ** 31
-        return quotient if -inf <= quotient <= inf - 1 else inf - 1
+                quotient += increment
+                multi_divisor = multi_divisor << 1  # double it
+                increment = increment << 1
+        return quotient if pos else -quotient
 
 
-if __name__ == "__main__":
-    print(Solution().divide(10, 3))
+print(Solution().divide(10, 3))
